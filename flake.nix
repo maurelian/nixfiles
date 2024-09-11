5d2cee7801a53671a07c343311036ba0867251a2
Maurelian <maurelian@protonmail.ch>
2024-09-11 15:11:18 -0400

    Simplify flake configuration

--- flake.nix
+++ flake.nix
@@ -1,49 +1,24 @@
-# ~/.config/nix/flake.nix
-
 {
   description = "My system configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin = {
-        url = "github:LnL7/nix-darwin";
-        inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = {pkgs, ... }: {
-
-        services.nix-daemon.enable = true;
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility. please read the changelog
-        # before changing: `darwin-rebuild changelog`.
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        # If you're on an Intel system, replace with "x86_64-darwin"
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        # Declare the user that will be running `nix-darwin`.
-        users.users.maurelian= {
+  outputs = { self, darwin, nixpkgs }: {
+    darwinConfigurations."MacBook-Pro-13.local" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ({ pkgs, ... }: {
+          users.users.maurelian = {
             name = "maurelian";
             home = "/Users/maurelian";
-        };
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true;
-        environment.systemPackages = [ pkgs.neofetch pkgs.vim ];
-    };
-  in
-  {
-    darwinConfigurations."MacBook-Pro-13.local" = nix-darwin.lib.darwinSystem {
-      modules = [
-         configuration
+          };
+        })
       ];
     };
   };
