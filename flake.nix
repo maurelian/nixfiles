58a25bb32426b94aea046a26bde159f0e3efb1c8
Maurelian <maurelian@protonmail.ch>
2024-09-18 12:29:01 -0400

    Partially working
    On this commit, I can run `home-manager switch`, but not
    `darwin-rebuild`.

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+      configuration = { pkgs, ... }: {
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
+      };
     in {
       homeConfigurations.maurelian = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -24,9 +29,17 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#MacBook-Pro-13.system
+      darwinConfigurations."MacBook-Pro-13.system" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
+      # darwinConfigurations."MacBook-Pro-13" = nix-darwin.lib.darwinSystem {
+      #   modules = [ configuration ];
+      # };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."MacBook-Pro-13.system".pkgs;
     };
 }
