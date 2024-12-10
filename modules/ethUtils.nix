7bccb7ff76cde1f68f4b893d964308a29a374934
Maurelian <maurelian@protonmail.ch>
2024-12-10 10:42:16 -0500

    work on ethUtils fish version

--- modules/ethUtils.nix
+++ modules/ethUtils.nix
@@ -2,11 +2,11 @@
 # source this in your .bashrc or .zshrc file with `. ~/.ethrc`
 {
   interface = ''
-    if [[ $1 == 0x* ]]; then
-      cast interface "$1" -c ''${2:-mainnet} --etherscan-api-key ''${3:-$ETHERSCAN_API_KEY}
+    if string match -q "0x*" $argv[1]
+        cast interface "$argv[1]" -c $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
     else
-      cast interface <(forge inspect "$1" abi)
-    fi
+        cast interface (forge inspect "$argv[1]" abi | psub)
+    end
   '';
   checksum =''
     cast --to-checksum-address "$1"
