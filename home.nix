9cc3552e9912ab4129cbc2b55f9537fd32c9c033
Maurelian <maurelian@protonmail.ch>
2026-02-19 13:10:43 -0500

    fix: correct XDG_CONFIG_HOME path to avoid breaking gh config lookup
    Was set to $HOME instead of $HOME/.config, causing tools like gh
    to look for config in the wrong directory.

--- home.nix
+++ home.nix
@@ -125,7 +125,7 @@ in
       set -x TENDERLY_USERNAME oplabs; set -x TENDERLY_PROJECT op-mainnet
       set -x RIPGREP_CONFIG_PATH $HOME/.ripgreprc
       set GPG_TTY $(tty)
-      set -x XDG_CONFIG_HOME $HOME
+      set -x XDG_CONFIG_HOME $HOME/.config
 
       # Set up npm global directory outside of Nix management
       set -x NPM_GLOBAL $HOME/.npm-global
